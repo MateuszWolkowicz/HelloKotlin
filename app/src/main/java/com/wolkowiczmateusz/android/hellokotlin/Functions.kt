5570969ed5ca8986f0edc14a5b46ed7ffd7251c1
Your Name <you@example.com>
2018-11-14 19:37:44 +0100

    Prepare version 1.2.0

---
+++ app/src/main/java/com/wolkowiczmateusz/android/hellokotlin/Functions.kt
@@ -0,0 +1,9 @@
+package com.wolkowiczmateusz.android.hellokotlin
+
+import android.widget.Toast
+
+class Functions {
+    fun main (args: Array<String>) {
+        println("Hello ${args[0]} ")
+    }
+}
