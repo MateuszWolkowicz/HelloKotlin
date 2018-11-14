758138568b3930d961da810d04928a4c4b82cbbc
Your Name <you@example.com>
2018-11-14 22:21:09 +0100

    Prepare version 1.3.0

--- app/src/main/java/com/wolkowiczmateusz/android/hellokotlin/Functions.kt
+++ app/src/main/java/com/wolkowiczmateusz/android/hellokotlin/Functions.kt
@@ -1,9 +1,35 @@
 package com.wolkowiczmateusz.android.hellokotlin
 
-import android.widget.Toast
+import java.util.*
 
 class Functions {
-    fun main (args: Array<String>) {
+    fun main(args: Array<String>) {
         println("Hello ${args[0]} ")
+        dayOfWeek()
+    }
+
+    fun dayOfWeek() {
+        println("What day is it today ?")
+        var day: Int = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
+
+        println(when (day) {
+            1 -> "Sunday"
+            2 -> "Monday"
+            3 -> "Tuesday"
+            4 -> "Wednesday"
+            5 -> "Thursday"
+            6 -> "Friday"
+            7 -> "Saturday"
+            else -> "Time has stopped"
+        })
+
+        when (day) {
+            1 -> show("Monday")
+            else -> show("other ")
+        }
+    }
+
+    private fun show(s: String) {
+        println(s + "day of the week")
     }
 }
