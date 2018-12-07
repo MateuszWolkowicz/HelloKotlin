75420fc3c79687f0e346f0b891d2f3ec6b6abfdf
MateuszWolkowicz <wolkowicz.mateusz@gmail.com>
2018-12-07 10:24:11 +0100

    advanced generics in kotlin

--- app/src/main/java/com/wolkowiczmateusz/android/hellokotlin/generics/Generics.kt
+++ app/src/main/java/com/wolkowiczmateusz/android/hellokotlin/generics/Generics.kt
@@ -16,12 +16,48 @@ class LakeWater : WaterSupply(true) {
     }
 }
 
-class Aquarium<T : WaterSupply>(val waterSupply: T)
+class Aquarium<out T : WaterSupply>(val waterSupply: T) {
+    fun addWater(cleaner: Cleaner<T>) {
+        if (waterSupply.needsProcessed) {
+            cleaner.clean(waterSupply)
+        }
+        println("adding water frim $waterSupply")
+    }
 
-fun genericsExample() {
-    val aquarium = Aquarium<TapWater>(TapWater())
-//    val aquarium = Aquarium(TapWater()) //the same
-    aquarium.waterSupply.addChemicalCleaners()
-//    val aquarium2 = Aquarium<TapWater>("string") //not possible if T: WaterSupply
+  inline  fun <reified R: WaterSupply> hasWaterSupplyOfType() =  waterSupply is R
 
 }
+
+
+interface Cleaner<in T : WaterSupply> {
+    fun clean(waterSupply: T)
+}
+
+class TapWaterCleaner : Cleaner<TapWater> {
+    override fun clean(waterSupply: TapWater) {
+        waterSupply.addChemicalCleaners()
+    }
+}
+
+fun addItemTo(aquarium: Aquarium<WaterSupply>) = println("item added")
+
+fun <T: WaterSupply> isWaterClean(aquarium: Aquarium<T>) {
+    println("aquarium water is clean: ${aquarium.waterSupply.needsProcessed}")
+}
+
+inline fun <reified  T: WaterSupply> WaterSupply.isOfType() = this is T
+
+fun genericsExample() {
+    val cleaner = TapWaterCleaner()
+//    val aquarium = Aquarium<TapWater>(TapWater())
+    val aquarium = Aquarium(TapWater()) //the same
+    aquarium.addWater(cleaner) // need in
+    addItemTo(aquarium) // need out
+    aquarium.waterSupply.addChemicalCleaners()
+//    val aquarium2 = Aquarium<TapWater>("string") //not possible if T: WaterSupply
+    isWaterClean(aquarium)
+    aquarium.hasWaterSupplyOfType<TapWater>() // true
+    aquarium.waterSupply.isOfType<LakeWater>() // false
+}
+
+
