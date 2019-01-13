0af22d7790608548eb722fbe7ce9766a7929f5c6
MateuszWolkowicz <wolkowicz.mateusz@gmail.com>
2019-01-14 00:41:48 +0100

    Val vs Const

--- app/src/main/java/com/wolkowiczmateusz/android/hellokotlin/constvsval/ConstVsVal.kt
+++ app/src/main/java/com/wolkowiczmateusz/android/hellokotlin/constvsval/ConstVsVal.kt
@@ -1,10 +1,17 @@
 package com.wolkowiczmateusz.android.hellokotlin.constvsval
 
+
+/*
+By default val properties are set at runtime. Adding a const modifier on a val would make a compile-time constant.
+A const cannot be used with a var or on its own.
+A const is not applicable on a local variable.
+*/
+
 const val rocks = 3
 val number = 5
 const val num = 5
 
-fun complexFunctionCall(){}
+fun complexFunctionCall() {}
 
 val result = complexFunctionCall()
 //const val result2 = complexFunctionCall() // can't do that  its set on compile time and we can't change it
@@ -12,9 +19,10 @@ val result = complexFunctionCall()
 const val CONSTANT = "top=level constant"
 
 //singletone
-object Constants{
-    const  val CONSTANT2 = "object constant"
+object Constants {
+    const val CONSTANT2 = "object constant"
 }
+
 val foo = Constants.CONSTANT2
 
 // no constant inside a class. We need to use wrapper companion objecy
